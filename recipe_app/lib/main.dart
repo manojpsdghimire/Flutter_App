eabda17bfb82dfd911f00cfa1554cf2380d4f14b
Manoj Ghimire <46035799+manojpsdghimire@users.noreply.github.com>
2019-08-05 10:32:26 -0500

    Basic Tutorial

--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import './screen/home.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-        title: "Hellow Flutter App",
-        home: new Material(
-            color: Colors.deepPurple,
-            child: Center(
-              child: Text(
-                "Hello Flutter!",
-                textDirection: TextDirection.ltr,
-              ),
-            ))),
-  );
+void main() => runApp( new HelloFlutterApp());
+class HelloFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:false ,
+      title: "Hellow Flutter App",
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Title in App Bar"),
+          ),
+          body: home()
+          ),
+    );
+  }
 }
